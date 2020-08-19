40017f38c9e3f1cc550f9a1c6eb94659ecbd9bb3
isblanco <isblancoc@gmail.com>
2020-08-20 00:46:28 +0200

    creates my first Capstone file

---
+++ Capstone_v01.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook for the DataScience Capstone project
+
+# In[ ]:
+
+
+get_ipython().system('conda install -c anaconda Numpy --yes')
+import pandas as pd
+import Numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project!")
+
+
+# In[ ]:
+
+
+
+
